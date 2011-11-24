f674fc1703782a85f026c1f2e0293d5096d801d6
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-24 15:06:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/horizontal-scrolling-announcement/tags/4.0@467077 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ horizontal-scrolling-announcement.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+function has_submit()
+{
+	if(document.form_hsa.hsa_text.value=="")
+	{
+		alert("Please enter the text.")
+		document.form_hsa.hsa_text.focus();
+		return false;
+	}
+	else if(document.form_hsa.hsa_status.value=="")
+	{
+		alert("Please select the display status.")
+		document.form_hsa.hsa_status.focus();
+		return false;
+	}
+	else if(document.form_hsa.hsa_order.value=="")
+	{
+		alert("Please enter the display order, only number.")
+		document.form_hsa.hsa_order.focus();
+		return false;
+	}
+	else if(isNaN(document.form_hsa.hsa_order.value))
+	{
+		alert("Please enter the display order, only number.")
+		document.form_hsa.hsa_order.focus();
+		return false;
+	}
+}
+
+function _hsadelete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.frm_hsa.action="options-general.php?page=horizontal-scrolling-announcement/horizontal-scrolling-announcement.php&AC=DEL&DID="+id;
+		document.frm_hsa.submit();
+	}
+}	
+
+function _hsa_redirect()
+{
+	window.location = "options-general.php?page=horizontal-scrolling-announcement/horizontal-scrolling-announcement.php";
+}
