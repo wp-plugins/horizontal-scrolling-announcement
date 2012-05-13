028b455fa295080efae7daf7efe943c006d4c08f
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-13 14:13:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/horizontal-scrolling-announcement/trunk@543764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,15 @@
+<?php
+function help()
+{
+?>
+<br />
+<strong>Plugin configuration option</strong>
+<ol>
+<li>Drag and drop the widget</li>
+<li>Add directly in the theme</li>
+<li>Short code for pages and posts</li>
+</ol>
+Note: Check official website for more info <a href="http://www.gopiplus.com/work/2010/07/18/horizontal-scrolling-announcement/" target="_blank">click here</a>
+<?php
+}
+?>
